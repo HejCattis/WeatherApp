dbd9ecf972dcda915c8b9f9b43e6975dfbf11443
Cattis Gustafsson <cattis@hejdev.se>
2023-04-19 11:58:06 +0200

    Test env

--- WeatherApp/src/App.tsx
+++ WeatherApp/src/App.tsx
@@ -1,9 +1,10 @@
 import { useState } from 'react'
 import './App.css'
 
+
 function App() {
   const [count, setCount] = useState(0)
-
+  // console.log("Message from .env:" + import.meta.env.VITE_APIkey)
   return (
     <div>
       <h1>Cattis Väderapp</h1>
