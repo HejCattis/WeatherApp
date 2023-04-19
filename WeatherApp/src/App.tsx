a7c53a69d420b38e5a58e22d3a21dc09c76823c3
Cattis Gustafsson <cattis@hejdev.se>
2023-04-19 12:01:00 +0200

    Removed vite logo and env console

--- WeatherApp/src/App.tsx
+++ WeatherApp/src/App.tsx
@@ -4,7 +4,6 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-  console.log("Message from .env:" + import.meta.env.VITE_APIkey)
   return (
     <div>
       <h1>Cattis Väderapp</h1>
