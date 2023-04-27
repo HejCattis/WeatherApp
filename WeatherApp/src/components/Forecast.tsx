18d52bd3e5b472a27b5d5753bd16d8d831865b36
Cattis Gustafsson <cattis@hejdev.se>
2023-04-27 20:39:55 +0200

    Added style

--- WeatherApp/src/components/Forecast.tsx
+++ WeatherApp/src/components/Forecast.tsx
@@ -5,20 +5,32 @@ import { getWeatherIcon } from "../utils/getWeatherIcon";
 function Forecast() {
   const { forecastData } = useStoreWeather();
 
-  const forecast = forecastData?.list.filter(({ dt_txt }) => new Date(dt_txt) >= new Date(Date.now() - 4 * 60 * 60 * 1000)).map(({ dt_txt, main: { temp }, weather: [{ main }] }) => ({
+  const forecast = forecastData?.list.filter(({ dt_txt }) => new Date(dt_txt) >= new Date(Date.now() - 4 * 60 * 60 * 1000)).map(({ dt_txt, main: { temp }, weather: [{ main }] }) => {
+    const dateTime = new Date(dt_txt);
+    const now = new Date();
+    const isNow = Math.abs(dateTime.getTime() - now.getTime()) <= 1.5 * 60 * 60 * 1000;
+    
+    const dateTimeString = isNow
+      ? "now"
+      : dateTime.toLocaleDateString() === now.toLocaleDateString()
+      ? dateTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false })
+      : `${dateTime.toLocaleDateString('en-GB', { day: 'numeric', month: 'numeric' })} ${dateTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false })}`;
+    
+    return {
       temperature: Math.round(temp),
       main,
-      dateTime: (new Date(dt_txt).toLocaleDateString() === new Date().toLocaleDateString() ? new Date(dt_txt).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false }) : `${new Date(dt_txt).toLocaleDateString('en-GB', { day: 'numeric', month: 'numeric' })} ${new Date(dt_txt).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false })}`),
-    }));
-    
+      dateTime: dateTimeString,
+    };
+  });
+  
   return (
     <div className="forecast-container">
       {forecast?.map(({ dateTime, main, temperature }, index) => (
-        <div key={index} style={{ marginRight: '10px' }}>
+        <section key={index}>
           <div>{getWeatherIcon(main)}</div>
-          <div>{`${temperature}°C`}</div>
-          <div>{dateTime}</div>
-        </div>
+          <p>{`${temperature} °C`}</p>
+          <p className="forecast-time">{dateTime}</p>
+        </section>
       ))}
     </div>
   );
