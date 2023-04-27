d3127e3fbbef607bb00c2a6e5862d944e25db1a1
Cattis Gustafsson <cattis@hejdev.se>
2023-04-27 22:30:54 +0200

    Added "now"

--- WeatherApp/src/components/Forecast.tsx
+++ WeatherApp/src/components/Forecast.tsx
@@ -3,26 +3,32 @@ import useStoreWeather from "../store/storeWeather";
 import { getWeatherIcon } from "../utils/getWeatherIcon";
 import { formatTime } from "../utils/formatTimeUtils";
 
-
 function Forecast() {
   const { forecastData } = useStoreWeather();
 
   const forecast = forecastData?.list
-  .filter(({ dt_txt }) => new Date(dt_txt) >= new Date(Date.now() - 4 * 60 * 60 * 1000))
-  .map(({ dt_txt, main: { temp }, weather: [{ main }] }) => {
-    const date = new Date(dt_txt);
-    const isToday = date.toLocaleDateString() === new Date().toLocaleDateString();
-    const dateTime = isToday
-      ? `${formatTime(date.getTime() / 1000)}`
-      : `${date.toLocaleDateString('en-GB', { day: 'numeric', month: 'numeric' })} ${formatTime(date.getTime() / 1000)}`;
-    return {
-      temperature: Math.round(temp),
-      main,
-      dateTime,
-    };
-  });
+    .filter(({ dt_txt }) => new Date(dt_txt) >= new Date(Date.now() - 5 * 60 * 60 * 1000))
+    .map(({ dt_txt, main: { temp }, weather: [{ main }] }) => {
+      const dateTime = new Date(dt_txt);
+      const now = new Date();
+      let isNow = false;
+      if (Math.abs(dateTime.getTime() - now.getTime()) <= 1.5 * 60 * 60 * 1000) {
+        isNow = true;
+      }
 
-  
+      const dateTimeString = isNow
+        ? <span className="forecast-time now">Now</span>
+        : dateTime.toLocaleDateString() === now.toLocaleDateString()
+        ? formatTime(dateTime.getTime() / 1000)
+        : `${dateTime.toLocaleDateString('en-GB', { day: 'numeric', month: 'numeric' })} ${formatTime(dateTime.getTime() / 1000)}`;
+
+      return {
+        temperature: Math.round(temp),
+        main,
+        dateTime: dateTimeString,
+      };
+    });
+
   return (
     <div className="forecast-container">
       {forecast?.map(({ dateTime, main, temperature }, index) => (
