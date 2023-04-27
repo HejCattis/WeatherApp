a2217a59fd11651e0056095f9d2b7af565a274b0
Cattis Gustafsson <cattis@hejdev.se>
2023-04-27 19:05:02 +0200

    Shorten the code

--- WeatherApp/src/components/Forecast.tsx
+++ WeatherApp/src/components/Forecast.tsx
@@ -1,40 +1,22 @@
+import "../style/Forecast.css";
 import useStoreWeather from "../store/storeWeather";
 import { getWeatherIcon } from "../utils/getWeatherIcon";
 
 function Forecast() {
   const { forecastData } = useStoreWeather();
 
-  // Filter out the dates that are earlier than the current time minus 4 hours
-  const filteredForecastData = forecastData?.list.filter(
-    ({ dt_txt }) => new Date(dt_txt) >= new Date(Date.now() - 4 * 60 * 60 * 1000)
-  );
-
-  // Map the filtered forecast data to weather data
-  const weatherData = filteredForecastData?.map(({ dt_txt, main: { temp }, weather: [{ main }] }) => {
-    const dateTime = new Date(dt_txt);
-    const now = new Date();
-    const start = new Date(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours() - (now.getHours() % 3), 0, 0);
-    const isNow = Math.abs(dateTime.getTime() - now.getTime()) <= 1.5 * 60 * 60 * 1000;
-
-    const dateTimeString = isNow
-      ? "now"
-      : dateTime.toLocaleDateString() === now.toLocaleDateString()
-      ? dateTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false })
-      : `${dateTime.toLocaleDateString('en-GB', { day: 'numeric', month: 'numeric' })} ${dateTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false })}`;
-
-    return {
+  const forecast = forecastData?.list.filter(({ dt_txt }) => new Date(dt_txt) >= new Date(Date.now() - 4 * 60 * 60 * 1000)).map(({ dt_txt, main: { temp }, weather: [{ main }] }) => ({
       temperature: Math.round(temp),
       main,
-      dateTime: dateTimeString,
-    };
-  });
-
+      dateTime: (new Date(dt_txt).toLocaleDateString() === new Date().toLocaleDateString() ? new Date(dt_txt).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false }) : `${new Date(dt_txt).toLocaleDateString('en-GB', { day: 'numeric', month: 'numeric' })} ${new Date(dt_txt).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false })}`),
+    }));
+    
   return (
-    <div style={{ display: 'flex', overflowX: 'scroll', maxWidth: '100%' }}>
-      {weatherData?.map(({ dateTime, main, temperature }, index) => (
+    <div className="forecast-container">
+      {forecast?.map(({ dateTime, main, temperature }, index) => (
         <div key={index} style={{ marginRight: '10px' }}>
           <div>{getWeatherIcon(main)}</div>
-          <div>{temperature}°C</div>
+          <div>{`${temperature}°C`}</div>
           <div>{dateTime}</div>
         </div>
       ))}
