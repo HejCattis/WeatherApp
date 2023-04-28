5a6d79acd88b3b42c726e14247f352f83c86ba15
Cattis Gustafsson <cattis@hejdev.se>
2023-04-28 08:18:32 +0200

    Set up of saved page

--- WeatherApp/src/pages/SavedPage.tsx
+++ WeatherApp/src/pages/SavedPage.tsx
@@ -1,12 +1,11 @@
 import Header from "../components/Header"
 
 function SavedPage() {
-return (
-    <div>
-        <Header title="Saved location" text="The locations is only saved in the session"></Header>
-    </div>
-
-)
+    return (
+        <div>
+            <Header title="Saved location" text="The locations is only saved in the session"></Header>
+        </div>
+    )
 }
 
 export default SavedPage
