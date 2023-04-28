77ef40ed3b259056f5cb890a4c9b728b5f948dd6
Cattis Gustafsson <cattis@hejdev.se>
2023-04-28 23:51:19 +0200

    Solved problem with double rerenders

--- WeatherApp/src/main.tsx
+++ WeatherApp/src/main.tsx
@@ -26,8 +26,7 @@ const router = createBrowserRouter([
   },
 ]);
 
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>
 )
--- WeatherApp/src/pages/SearchPage.tsx
+++ WeatherApp/src/pages/SearchPage.tsx
@@ -1,9 +1,17 @@
+import { useEffect } from "react";
 import Navbar from "../components/Navbar";
 import { useParams } from 'react-router';
 
 
 function SearchPage() {
     const { query } = useParams();
+    useEffect(()=>{
+        /*
+        Query logic
+        */
+        console.log('i fire once');
+  },[]);
+
 
     return(
         <div>
